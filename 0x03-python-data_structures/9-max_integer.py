009c57f40bec548934fec92dae4945a23797b3dd
Eveline-nyd <gertnadzua@gmail.com>
2024-01-22 06:27:11 -0800

    finding maximum integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        maximum = my_list[0]
+        for m in range(len(my_list)):
+            if my_list[m] > maximum:
+                maximum = my_list[m]
+                return maximum
