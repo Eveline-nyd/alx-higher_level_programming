e2b2080b0ae3f4c4ec854c45fc798382f20bc6c3
Eveline-nyd <gertnadzua@gmail.com>
2024-01-22 06:08:01 -0800

    adding tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for m in matrix:
             for n in m:
-                print("{:d}".format(n), end=''if n != m[-1] else '')
+                print("{:d}".format(n), end='' if n != m[-1] else '')
                 print()
