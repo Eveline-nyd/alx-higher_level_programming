addc6f7e596c2d9d81fc24006bff9ae8fdce58af
Eveline-nyd <gertnadzua@gmail.com>
2024-02-17 19:16:24 -0800

    addition of integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,46 +1,17 @@
 #!/usr/bin/python3
+"""Defines an integer addition function."""
+
+
 def add_integer(a, b=98):
-    """
-    Add two integers.
+    """Return the integer addition of a and b.
 
-    Parameters:
-    - a: int or float, first number to add
-    - b: int or float, second number to add (default is 98)
-
-    Returns:
-    - int, the sum of a and b
+    Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-    - TypeError: If a or b is not an integer or float
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer, a must be an integer")
-
-    a = int(a)
-    b = int(b)
-
-    return a + b
-
-
-def run_tests():
-     with open('tests/0-add_integer.txt', 'r') as f:
-        for line in f:
-            test_input, expected_output = line.strip().split(';')
-            test_input = [
-                float(x) if '.' in x else int(x) for x in test_input.split(',')
-            ]
-            result = add_integer(*test_input)
-            try:
-                expected_output = float(
-                    expected_output) if '.' in expected_output else int(
-                        expected_output)
-                assert result == expected_output
-                print(f"Test passed: {test_input} => {expected_output}")
-            except AssertionError:
-                print(
-                    f"Test failed: {test_input} => {expected_output}, got {result}"
-                )
-
-
-if __name__ == '__main__':
-    run_tests()
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
