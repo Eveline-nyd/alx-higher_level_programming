2384e2d4c668f2c1f083f9eab842662ab5ca89c8
Eveline-nyd <gertnadzua@gmail.com>
2024-02-22 04:05:05 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
- """
-    Read the contents of a text file and print it to stdout.
+"""Read the contents of a text file and print it to stdout."""
 
-    Args:
-    - filename (str): The path to the file to be read. Defaults to an empty string.
 
-    Returns:
-    - None
-    """
+
 def read_file(filename=""):
-    with open("my_file_0.txt", mode="r", encoding="utf-8") as file:
-            print(file.read(), end ="")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open("my_file_0.txt", mode="r", encoding="utf-8") as f:
+            print(f.read(), end ="")
