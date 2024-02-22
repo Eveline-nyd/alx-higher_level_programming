8807973c7ea98cd4a38737545b5e00babb7807a6
Eveline-nyd <gertnadzua@gmail.com>
2024-02-22 12:45:50 -0800

    student toJSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -27,5 +27,5 @@ class Student:
             attrs (list): (Optional) The attributes to represent.
         """
         if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
-            return {m: getattr(self, m) for m in attrs if hasattr(self, m)}
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
