30b2878616d3d5c8804b6cf37dc50c37fda517ed
Eveline-nyd <gertnadzua@gmail.com>
2024-02-22 04:10:37 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,8 +2,7 @@
 """Read the contents of a text file and print it to stdout."""
 
 
-
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open("my_file_0.txt", mode="r", encoding="utf-8") as f:
-            print(f.read(), end ="")
+        print(f.read(), end="")
